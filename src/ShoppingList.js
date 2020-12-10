795318881255959469409b07a6ec5ae674723f7c
Cory Fladhammer <cjfladham@hotmail.com>
2020-12-09 21:28:31 -0600

    working shit

---
+++ src/ShoppingList.js
@@ -0,0 +1,24 @@
+import React from 'react'
+
+import ShoppingItem from './ShoppingItem'
+
+function ShoppingList(props) {
+    return (
+        <ul>
+            {props.items.map((item, i) =>
+                <ShoppingItem
+                    key={i}
+                    item={item}
+                    onDeleteItem={props.onDeleteItem}
+                    onCheckItem={props.onCheckItem}
+                />
+            )}
+        </ul>
+    )
+}
+
+ShoppingList.defaultProps = {
+    items: []
+}
+
+export default ShoppingList
